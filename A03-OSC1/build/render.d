46445fcec382e79f72bfa89325ecb59f0ad5e31e
John B <jrbcomwork@yahoo.com>
2022-06-06 16:56:11 -0400

    Simple OSC reader for testing

---
+++ A03-OSC1/build/render.d
@@ -0,0 +1,22 @@
+/root/Bela/projects/A03-OSC1/build/render.o: \
+  /root/Bela/projects/A03-OSC1/render.cpp include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  include/Utilities.h libraries/OscSender/OscSender.h \
+  libraries/OscReceiver/OscReceiver.h include/oscpkt.hh
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+include/Utilities.h:
+
+libraries/OscSender/OscSender.h:
+
+libraries/OscReceiver/OscReceiver.h:
+
+include/oscpkt.hh:
